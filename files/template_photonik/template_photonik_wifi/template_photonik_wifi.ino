951f2666963f21ac89e802519d9363ea27516d7b
Jan Wirwahn <jan.wirwahn@uni-muenster.de>
2015-02-08 17:26:24 +0100

    Fixed upload bug for WiFi setup

--- files/template_photonik/template_photonik_wifi/template_photonik_wifi.ino
+++ files/template_photonik/template_photonik_wifi/template_photonik_wifi.ino
@@ -6,100 +6,105 @@ Homepage: http://www.sensebox.de
 Author: Jan Wirwahn
 Note: Sketch for SB-Photonik-WiFi
 */
-
 #include <Wire.h>
 #include <SPI.h>
-#include <EthernetV2_0.h>
+#include <Adafruit_CC3000.h>
 #include <DHT.h>
 #include <Barometer.h>
 #include <Digital_Light_TSL2561.h>
-#include <avr/wdt.h>
+//#include <avr/wdt.h>
 
 //SenseBox ID
 
 //Sensor IDs
 
+// WLAN parameters
+#define WLAN_SSID       "WiFi_Name"           // cannot be longer than 32 characters!
+#define WLAN_PASS       "WiFi_Password"
+// Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
+#define WLAN_SECURITY   WLAN_SEC_WPA2  
+
+// These are the interrupt and control pins
+#define ADAFRUIT_CC3000_IRQ   3  
+#define ADAFRUIT_CC3000_VBAT  5
+#define ADAFRUIT_CC3000_CS    10
+#define IDLE_TIMEOUT_MS  3000  
+// Use hardware SPI for the remaining pins
+// On an UNO, SCK = 13, MISO = 12, and MOSI = 11
+Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT,
+                                         SPI_CLOCK_DIVIDER); // you can change this clock speed  
+//Server values
+#define WEBSITE "www.opensensemap.org"
+uint32_t ip = 2159055603;
+
 //Sensor pin settings
 #define UVPIN A0
 #define DHTPIN A1
 #define DHTTYPE DHT11
-//Ethernet shield settings
-#define W5200_CS 10
-#define SDCARD_CS 4
-
-//Network settings
-IPAddress ip(192,168,178,111);//If DHCP is disabled, specify an IP according to your network settings
-byte mac[] = { 0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 };
-char server[] = "opensensemap.org";
-EthernetClient client;
 
 String currentSensorId = TEMPERATURESENSOR_ID;
-float temperature;
-float humidity;
-float pressure;
+float temperature, humidity, pressure;
+unsigned long lux;
+int analogUvLight,contLen;
+
+short phenomenonCount = 5; //1=temp,2=humi,3=pressure,4=lux,5=UV
+int sampleType = 1; //begin with temperature
+boolean uploadSuccess = true;
+String sensorSample,jsonData;
 
 DHT dht(DHTPIN, DHTTYPE);
 Barometer barometer;
 
-//Variables for time sync and upload intervall
-const unsigned long postingInterval = 10*1000*60; //1 minute
-unsigned long lastConnectionTime = 0;
-boolean lastConnected = false;
-boolean uploadSuccess = true;
-String sensorSample;
-int sampleType = 1; //1=temp,2=humi,3=pressure,4=lux,5=UV
-
-void setup(){
-  Wire.begin();
-  Serial.begin(9600);
-  //Setup chip select and deselect the SD card
-  pinMode(SDCARD_CS,OUTPUT);
-  digitalWrite(SDCARD_CS,HIGH);
-  delay(500);
-  // start the Ethernet connection 
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP.");
-    //If DHCP is disabled, use static IP
-    Ethernet.begin(mac, ip);
-  }else Serial.println("DHCP setup successful.");
-  //Print the IP to serial port
-  Serial.print("My IP address: ");
-  for (byte thisByte = 0; thisByte < 4; thisByte++) {
-    // print the value of each byte of the IP address:
-    Serial.print(Ethernet.localIP()[thisByte], DEC);
-    Serial.print("."); 
+void setup(void)
+{
+  Serial.begin(115200);
+  Serial.println(F("\nInitializing WiFi..."));
+  if (!cc3000.begin())
+  {
+    Serial.println(F("failed! Check your wiring?"));
+    while(1);
   }
-  Serial.println("STARTING UP");
+  
+  Serial.print(F("\nAttempting to connect to ")); Serial.println(WLAN_SSID);
+  if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
+    Serial.println(F("Failed!"));
+    while(1);
+  }
+   
+  Serial.println(F("Connected!"));
+  
+  /* Wait for DHCP to complete */
+  Serial.println(F("Requesting DHCP"));
+  while (!cc3000.checkDHCP())
+  {
+    delay(100); // ToDo: Insert a DHCP timeout!
+  }
+  ip = 0;
+  // Try looking up the website's IP address
+  Serial.print(WEBSITE); Serial.print(F(" -> "));
+  while (ip == 0) {
+    if (! cc3000.getHostByName(WEBSITE, &ip)) {
+      Serial.println(F("Couldn't resolve!"));
+    }
+    delay(500);
+  }
+  cc3000.printIPdotsRev(ip);  
+  Serial.println("\nSTARTING UP");
   barometer.init();
   dht.begin();
   TSL2561.init();
 }
 
-void loop(){
-  // if there's incoming data from the net connection.
-  // send it out the serial port.  This is for debugging purposes only
-  if (client.available()) {
-    char c = client.read();
-    Serial.print(c);
-  }
-  // if there's no net connection, but there was one last time
-  // through the loop, then stop the client:
-  if (!client.connected() && lastConnected) {
-    Serial.println();
-    Serial.println();
-    Serial.println("...stopping client!");
-    client.stop();
-  }  
-  //If last upload was successful, switch case to select the next sensor
+void loop(void)
+{
   if (uploadSuccess){
-    Serial.println();
-    delay(1000);
+    Serial.print("Reading sensor...");
     sensorSample = "";
     switch (sampleType)
     {
       case 1:
-        //temperature = dht.readTemperature();
-        temperature = barometer.bmp085GetTemperature(barometer.bmp085ReadUT());
+        temperature = dht.readTemperature();
+        //temperature = barometer.bmp085GetTemperature(barometer.bmp085ReadUT());
         sensorSample = floatToString(temperature,0);
         currentSensorId = TEMPERATURESENSOR_ID;
         break;
@@ -121,25 +126,75 @@ void loop(){
         currentSensorId = LUXSENSOR_ID;
         break;
       case 5:
-        analogLight = analogRead(UVPIN);
-        sensorSample = (String)calcUVIndex(analogLight);
+        analogUvLight = analogRead(UVPIN);
+        sensorSample = (String)calcUVIndex(analogUvLight);
         currentSensorId = UVSENSOR_ID;
         break;
     }
+    Serial.println("done...................");
   }
-  // if you're not already connected, and 60 seconds have passed since
-  // your last connection, then connect again and send data: 
-  uploadSuccess = false;
-  if(!client.connected() && (millis() - lastConnectionTime > postingInterval)) {
-    Serial.println("-----------------------------------");
-    Serial.print("Connecting and posting sensor sample (case ");
-    Serial.print(sampleType);
-    Serial.print(")...");
-    Serial.println(); 
-    postObservation(sensorSample, currentSensorId, SENSEBOX_ID);
+
+  jsonData = "{\"value\":"; 
+  jsonData += sensorSample; 
+  jsonData += "}";
+  Serial.println(jsonData);
+  contLen = jsonData.length();
+  Adafruit_CC3000_Client client = cc3000.connectTCP(ip, 8000);
+  
+  if (client.connected()) {
+    /*
+    clientclient.print(F("GET "));
+    client.print(F("/"));
+    client.print(F(" HTTP/1.1\r\n"));
+    client.print(F("Host: ")); client.fastrprint(WEBSITE); .fastrprint(F("\r\n"));
+    www.print(F("\r\n"));
+    www.println();
+    */
+    client.print("POST /boxes/");
+    client.print(SENSEBOX_ID);
+    client.print("/"); 
+    client.print(currentSensorId); 
+    client.print(" HTTP/1.1\r\n"); 
+    client.println("Host: opensensemap.org");
+    client.println("Content-Type: application/json");   
+    client.print("Content-Length: "); 
+    client.println(contLen); 
+    client.println("Connection: close");
+    client.println(); 
+    client.print(jsonData); 
+    client.println(); 
+    Serial.println("done!");
+    uploadSuccess = true;
+  } else {
+    Serial.println(F("Connection failed"));    
+    uploadSuccess = false;
+    return;
   }
-  // store the state of the connection for next time through the loop
-  lastConnected = client.connected();
+
+  Serial.println(F("-------------------------------------"));
+  
+  /* Read data until either the connection is closed, or the idle timeout is reached. */ 
+  unsigned long lastRead = millis();
+  while (client.connected() && (millis() - lastRead < IDLE_TIMEOUT_MS)) {
+    while (client.available()) {
+      char c = client.read();
+      Serial.print(c);
+      lastRead = millis();
+    }
+  }
+  client.close();
+  Serial.println(F("-------------------------------------"));
+  if (uploadSuccess){
+    if (sampleType == phenomenonCount) {
+      sampleType = 1;
+    }else sampleType++;
+  }
+  /* You need to make sure to clean up after yourself or the CC3000 can freak out */
+  /* the next time your try to connect ... */
+  Serial.println(F("\n\nDisconnecting"));
+//  cc3000.disconnect();
+
+ delay(1000);
 }
 
 String floatToString(float number, int precision)
@@ -149,7 +204,7 @@ String floatToString(float number, int precision)
   //only temperature (case 1) has a decimal place
   //if (sampleType == 1) prec = 1; else prec = 0;
   char tempChar[10];
-  dtostrf(number, 3, precision, tempChar);
+  dtostrf(number, 1, precision, tempChar);
   stringNumber += tempChar;
   return stringNumber;
 }
@@ -169,53 +224,4 @@ int calcUVIndex(int analogValue){
   else if (analogValue<221) uvi = 10;
   else if (analogValue<240) uvi = 11;
   return uvi;
-}
-
-//Method for posting a measurement to OSM server
-void postObservation(String measurement, String sensorId, String boxId)
-{  
-  //if (measurement=="") return;
-  wdt_enable(WDTO_8S);
-  //json must look like {"value":"12.5"} 
-  String valueJson = "{\"value\":"; 
-  valueJson += measurement; 
-  valueJson += "}";
-  Serial.println(valueJson);
-  //post observation to: http://opensensemap.org:8000/boxes/boxId/sensorId
-  // if you get a connection, report back via serial: 
-  wdt_reset();
-  if (client.connect(server, 8000)) 
-  {
-    Serial.print("connected..."); 
-    Serial.println();
-    // Make a HTTP Post request: 
-    client.print("POST /boxes/"); 
-    client.print(boxId);
-    client.print("/"); 
-    client.print(sensorId); 
-    client.println(" HTTP/1.1"); 
-    // Send the required header parameters 
-    client.println("Host:opensensemap.org"); 
-    client.println("Content-Type: application/json"); 
-    client.println("Connection: close");  
-    client.print("Content-Length: "); 
-    client.println(valueJson.length()); 
-    client.println(); 
-    client.print(valueJson); 
-    client.println(); 
-    Serial.println("done!");
-    uploadSuccess = true;
-    //Change case
-    if (sampleType == 5) {
-      sampleType = 1;
-      // remember the time that the connection was made or attempted
-      lastConnectionTime = millis();
-    }else sampleType++;
-  }else 
-  {
-    // stop the client if you couldn't make a connection !
-    Serial.println("failed...disconnecting.");
-    client.stop();
-  }
-  wdt_disable();
 }
