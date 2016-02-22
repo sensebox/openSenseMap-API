64b614c71e09a5e2cbf081c554adeba575fe3a8d
Christoph Kisfeld <spam@b-web.org>
2016-02-22 22:32:40 +0100

    Add default dbhost localhost

--- config/index.js
+++ config/index.js
@@ -1,11 +1,11 @@
 exports.targetFolder = '/var/OpenSenseMap-API/usersketches/';
 exports.imageFolder = '/var/www/OpenSenseMap/app/userimages/';
-exports.dbhost = '';
+exports.dbhost = 'localhost';
 exports.dbuser = '';
 exports.dbuserpass = '';
 
 exports.email = {};
-exports.email.host = '';
+exports.email.host = ''; // leave empty to not send emails
 exports.email.port = 465;
 exports.email.secure = true;
 exports.email.user = '';
