6ce363959f87abb5e247ca6c3bcde1da2e66da18
Travis-CI <travis@travis-ci.org>
2016-07-29 21:33:48 +0000

    Deployed by Travis

--- api_project.js
+++ api_project.js
@@ -1,14 +1,12 @@
 define({
-  "name": "openSenseMap API documentation",
-  "version": "0.0.1",
-  "description": "Documentation for openSenseMap to register and administer senseBoxes",
-  "title": "openSenseMap API docs title",
-  "url": "https://api.opensensemap.org",
+  "name": "",
+  "version": "0.0.0",
+  "description": "",
   "sampleUrl": false,
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-07-29T20:57:40.231Z",
+    "time": "2016-07-29T21:33:48.374Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
