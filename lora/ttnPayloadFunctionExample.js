67ddc2cd65e6f9501087344851707c1a6ff80469
ubergesundheit <ubergesundheit@googlemail.com>
2016-09-14 11:33:30 +0200

    add example function for ttn

---
+++ lora/ttnPayloadFunctionExample.js
@@ -0,0 +1,131 @@
+function decode (bytes) {
+  // bytes is of type Buffer.
+  'use strict';
+
+  var TEMPSENSOR_ID = '',
+    HUMISENSOR_ID = '',
+    PRESSURESENSOR_ID = '',
+    LUXSENSOR_ID = '',
+    UVSENSOR_ID = '';
+
+  var bytesToInt = function (bytes) {
+    var i = 0;
+    for (var x = 0; x < bytes.length; x++) {
+      i |= +(bytes[x] << (x * 8));
+    }
+    return i;
+  };
+
+  var uint8 = function (bytes) {
+    if (bytes.length !== uint8.BYTES) {
+      throw new Error('int must have exactly 1 byte');
+    }
+    return bytesToInt(bytes);
+  };
+  uint8.BYTES = 1;
+
+  var uint16 = function (bytes) {
+    if (bytes.length !== uint16.BYTES) {
+      throw new Error('int must have exactly 2 bytes');
+    }
+    return bytesToInt(bytes);
+  };
+  uint16.BYTES = 2;
+
+  var temperature = function (bytes) {
+    if (bytes.length !== temperature.BYTES) {
+      throw new Error('Temperature must have exactly 2 bytes');
+    }
+
+    var t = bytesToInt(bytes);
+    if ((bytes[1] & 0x60)) {
+      t = ~t + 1;
+    }
+
+    return t / 1e2;
+  };
+  temperature.BYTES = 2;
+
+  var humidity = function (bytes) {
+    if (bytes.length !== humidity.BYTES) {
+      throw new Error('Humidity must have exactly 2 bytes');
+    }
+
+    var h = bytesToInt(bytes);
+    return h / 1e2;
+  };
+  humidity.BYTES = 2;
+
+  var decode = function (bytes, mask, names) {
+
+    var maskLength = mask.reduce(function (prev, cur) {
+      return prev + cur.BYTES;
+    }, 0);
+    if (bytes.length < maskLength) {
+      throw new Error('Mask length is ' + maskLength + ' whereas input is ' + bytes.length);
+    }
+
+    names = names || [];
+    var offset = 0;
+    return mask
+      .map(function (decodeFn) {
+        var current = bytes.slice(offset, offset += decodeFn.BYTES);
+        return decodeFn(current);
+      })
+      .reduce(function (prev, cur, idx) {
+        prev[names[idx] || idx] = cur;
+        return prev;
+      }, {});
+  };
+
+  var bytesToSenseBoxJson = function (bytes) {
+    var json;
+
+    try {
+      json = decode(bytes,
+        [
+          temperature,
+          humidity,
+          uint16,
+          uint8,
+          uint16,
+          uint8,
+          uint16
+        ],
+        [
+          TEMPSENSOR_ID,
+          HUMISENSOR_ID,
+          PRESSURESENSOR_ID,
+          LUXSENSOR_ID + '_mod',
+          LUXSENSOR_ID + '_times',
+          UVSENSOR_ID + '_mod',
+          UVSENSOR_ID + '_times'
+        ]);
+
+      //temp
+      json[TEMPSENSOR_ID] = parseFloat(json[TEMPSENSOR_ID].toFixed(1));
+
+      //hum
+      json[HUMISENSOR_ID] = parseFloat(json[HUMISENSOR_ID].toFixed(1));
+
+      // pressure
+      json[PRESSURESENSOR_ID] = parseFloat(((json[PRESSURESENSOR_ID] / 81.9187) + 300).toFixed(1));
+
+      // lux
+      json[LUXSENSOR_ID] = (json[LUXSENSOR_ID + '_times'] * 255) + json[LUXSENSOR_ID + '_mod'];
+      delete json[LUXSENSOR_ID + '_times'];
+      delete json[LUXSENSOR_ID + '_mod'];
+
+      // uv
+      json[UVSENSOR_ID] = (json[UVSENSOR_ID + '_times'] * 255) + json[UVSENSOR_ID + '_mod'];
+      delete json[UVSENSOR_ID + '_times'];
+      delete json[UVSENSOR_ID + '_mod'];
+    } catch (e) {
+      json = { payload: bytes };
+    }
+
+    return json;
+  };
+
+  return bytesToSenseBoxJson(bytes);
+}
