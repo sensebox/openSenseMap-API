91910146e8a0d0b9ea97852fa95a7765a9acb0e8
ubergesundheit <ubergesundheit@googlemail.com>
2016-11-12 19:06:46 +0100

    add more eslint rules, refactor routes to controllers

--- .eslintrc.js
+++ .eslintrc.js
@@ -64,6 +64,97 @@ module.exports = {
     ],
     'space-before-blocks': [
       'error'
+    ],
+    'no-template-curly-in-string': [
+      'error'
+    ],
+    'block-scoped-var': [
+      'error'
+    ],
+    'complexity': [
+      'warn',
+      10
+    ],
+    'dot-location': [
+      'error',
+      'property'
+    ],
+    'no-else-return': 'error',
+    'no-floating-decimal': 'error',
+    'no-implicit-coercion': 'error',
+    'no-implicit-globals': 'error',
+    'no-implied-eval': 'error',
+    'no-lone-blocks': 'error',
+    'no-loop-func': 'error',
+    'no-multi-str': 'error',
+    'no-new-wrappers': 'error',
+    'no-new': 'error',
+    'no-return-assign': 'error',
+    'no-throw-literal': 'error',
+    'no-unmodified-loop-condition': 'error',
+    'no-useless-return': 'error',
+    'radix': [
+      'error',
+      'always'
+    ],
+    'yoda': 'error',
+    'callback-return': 'error',
+    'global-require': 'error',
+    'comma-spacing': [
+      'error',
+      { 'before': false, 'after': true }
+    ],
+    'comma-style': [
+      'error',
+      'last'
+    ],
+    'computed-property-spacing': [
+      'error',
+      'never'
+    ],
+    'eol-last': [
+      'error',
+      'always'
+    ],
+    'func-call-spacing': [
+      'error',
+      'never'
+    ],
+    'func-name-matching': [
+      'error',
+      'always'
+    ],
+    'func-style': [
+      'error',
+      'expression'
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'lines-around-directive': [
+      'error'
+    ],
+    'newline-before-return': 'error',
+    'newline-per-chained-call': 'error',
+    'no-lonely-if': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error',
+      'always'
+    ],
+    'operator-assignment': [
+      'error',
+      'never'
+    ],
+    'no-confusing-arrow': 'error',
+    'no-useless-computed-key': 'error',
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'prefer-template': 'error',
+    'template-curly-spacing': [
+      'error',
+      'never'
     ]
   }
 };
