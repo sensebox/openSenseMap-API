805404c8c8b2e55603b3437da99657c27808528d
Travis-CI <travis@travis-ci.org>
2016-11-30 14:59:49 +0000

    apidoc build from 0ac396149df6b47fcefe8a572bf181da2b5c5f1f by Travis

--- api_project.js
+++ api_project.js
@@ -21,7 +21,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-11-29T16:21:28.623Z",
+    "time": "2016-11-30T14:59:49.388Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
