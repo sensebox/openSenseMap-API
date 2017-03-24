56242de8b5deea25288bf843c69d2bc0ca04edf3
ubergesundheit <ubergesundheit@googlemail.com>
2017-03-24 09:54:16 +0100

    clean up products/sensorLayouts

--- products/index.js
+++
@@ -1,6 +0,0 @@
-exports.senseboxhome2014 = require('./sensebox.home.2014.js');
-exports.senseboxphotonikwifi = require('./sensebox.photonik.wifi.js');
-exports.senseboxphotonikethernet = require('./sensebox.photonik.ethernet.js');
-
-exports.senseboxhome = require('./sensebox.home.js');
-exports.senseboxbasic = require('./sensebox.basic.js');
