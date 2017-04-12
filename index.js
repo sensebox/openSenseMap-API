fc1e0192406314e48e7f34e8d903a0b08c7d2b14
ubergesundheit <ubergesundheit@googlemail.com>
2017-04-12 12:30:16 +0200

    make project easier to require

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const models = require('./lib/models'),
+  utils = require('./lib/utils'),
+  decoding = require('./lib/decoding');
+
+module.exports = {
+  models,
+  utils,
+  decoding
+};
