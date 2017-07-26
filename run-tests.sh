51920537ef3ad1a6187935e062cbb953184a6c4a
ubergesundheit <ubergesundheit@googlemail.com>
2017-07-26 11:49:01 +0200

    add parameters to run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -1,35 +1,54 @@
 #!/bin/bash
 set -euo pipefail
-IFS=$'\n\t'
 
 # Disclaimer: I am no bash wizard
 
 dont_clean_up=${dont_clean_up:-}
-dont_recreate=${dont_recreate:-}
+show_logs=${show_logs:-}
+
+cmd=${1:-}
+logs_service=${2:-}
+
+case "$cmd" in
+"build" )
+  docker-compose -p osemapitest -f tests-docker-compose.yml build osem-api
+  exit 0;
+  ;;
+"logs" )
+  shift # remove "logs" from $@
+  show_logs="true"
+  if [[ "$logs_service" == "all" ]]; then
+    logs_service=""
+  fi
+  ;;
+"dont_clean_up" )
+  dont_clean_up="true"
+esac
 
 mailer_tag=$(git rev-parse --abbrev-ref HEAD)
-export SENSEBOX_MAILER_TAG="$mailer_tag"
+if [[ "$mailer_tag" == "master" || "$mailer_tag" == "development" ]]; then
+  export SENSEBOX_MAILER_TAG="$mailer_tag"
+else
+  export SENSEBOX_MAILER_TAG="development"
+fi
 
 function cleanup {
+  if [[ -n "$show_logs" ]]; then
+    if [[ -z "$logs_service" ]]; then
+      docker-compose -p osemapitest -f tests-docker-compose.yml logs
+    else
+      docker-compose -p osemapitest -f tests-docker-compose.yml logs "$logs_service"
+    fi
+  fi
+
   if [[ -z "$dont_clean_up" ]]; then
     echo 'cleanup!'
-#    docker-compose -p osemapitest -f tests-docker-compose.yml logs osem-api mailer
     docker-compose -p osemapitest -f tests-docker-compose.yml down -v
   fi
 }
 trap cleanup EXIT
 
-#docker-compose -p osemapitest -f tests-docker-compose.yml up -d --force-rec db
-#if [[ -n "$dont_recreate" ]]; then
-#  echo 'no recreate'
-#  docker-compose -p osemapitest -f tests-docker-compose.yml up -d --remove-orphans --no-deps osem-api
-#else
-#  #docker-compose -p osemapitest -f tests-docker-compose.yml up -d --force-recreate --remove-orphans --no-deps --build  osem-api
-#  docker-compose -p osemapitest -f tests-docker-compose.yml up -d --force-recreate --remove-orphans --no-deps  osem-api
-#fi
-
 docker-compose -p osemapitest -f tests-docker-compose.yml down -v
-#docker-compose -p osemapitest -f tests-docker-compose.yml up -d --build --force-recreate --remove-orphans
 docker-compose -p osemapitest -f tests-docker-compose.yml up -d --force-recreate --remove-orphans
 
 docker-compose -p osemapitest -f tests-docker-compose.yml exec osem-api npm test
