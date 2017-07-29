64e4610a13c572a87a86e753811a581c60f9ea29
noerw <noerw@gmx.de>
2017-07-29 22:44:32 +0200

    check for schema version in db migration

--- migrations/1-1_mobile-boxes/0-mongoshell-refactor-boxlocation.js
+++ migrations/1-1_mobile-boxes/0-mongoshell-refactor-boxlocation.js
@@ -1,3 +1,22 @@
+/* eslint-disable */
+
+db = db.getSiblingDB('OSeM-api');
+
+// check schema version
+var collections = db.getCollectionNames();
+
+if (collections.indexOf("schemaVersion") === -1) {
+  print("Error: Unkown schema version. Exiting!");
+  quit();
+}
+
+var latestVersion = db.schemaVersion.find({}).sort({ schemaVersion: -1 }).limit(1).next();
+
+if (latestVersion.schemaVersion !== 3) {
+  print("Migration already applied... Exiting!");
+  quit();
+}
+
 var boxIter = db.boxes.find(),
   // stack all operations in bulks, and execute them at the end.
   boxesBulk = db.boxes.initializeUnorderedBulkOp();
@@ -24,3 +43,5 @@ while (boxIter.hasNext()) {
 }
 
 boxesBulk.execute();
+
+db.schemaVersion.updateOne({}, { $inc: { schemaVersion: 1 }});
