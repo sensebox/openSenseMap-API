62d5b1b9ebb110ee4594028790e43829f0f5d54b
ubergesundheit <ubergesundheit@googlemail.com>
2017-09-11 11:12:35 +0200

    only run tagging on master

--- .scripts/npm_tag-container.sh
+++ .scripts/npm_tag-container.sh
@@ -3,12 +3,12 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-#currentBranch=$(git branch | grep -e "^*" | cut -d' ' -f 2)
-#
-#if [ "$currentBranch" != "master" ]; then
-#  echo "Tags for containers can only be made from 'master' branch"
-#  exit 1
-#fi
+currentBranch=$(git branch | grep -e "^*" | cut -d' ' -f 2)
+
+if [ "$currentBranch" != "master" ]; then
+  echo "Tags for containers can only be made from 'master' branch"
+  exit 1
+fi
 
 # Extract latest git tag
 currentVersion=$(git tag -l --sort=-v:refname | head -n 1 | tr -d 'v')
