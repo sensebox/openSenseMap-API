85abb802ae62c1fa3277b2bbefb1307850275d0b
ubergesundheit <ubergesundheit@googlemail.com>
2018-02-05 15:00:47 +0100

    allow object spread in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,12 @@ module.exports = {
     es6: true,
     node: true
   },
+  parserOptions: {
+    ecmaVersion: 2018,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true
+    }
+  },
   extends: ['@sensebox/eslint-config-sensebox'],
   rules: {
     complexity: ['warn', 20]
