d9a890a0242b84d00bfc73c22cde98bcf2d0e29c
Matthias Pfeil <matthias.pfeil@wwu.de>
2018-09-19 14:05:25 +0200

    (api) change sensorTemplates documentation

--- packages/api/lib/controllers/boxesController.js
+++ packages/api/lib/controllers/boxesController.js
@@ -358,7 +358,7 @@ const getBox = async function getBox (req, res, next) {
  * @apiParam (RequestBody) {Location} location the coordinates of this senseBox.
  * @apiParam (RequestBody) {String="homeV2Lora","homeV2Ethernet","homeV2Wifi","homeEthernet","homeWifi","homeEthernetFeinstaub","homeWifiFeinstaub","luftdaten_sds011","luftdaten_sds011_dht11","luftdaten_sds011_dht22","luftdaten_sds011_bmp180","luftdaten_sds011_bme280"} [model] specify the model if you want to use a predefined senseBox model, autocreating sensor definitions.
  * @apiParam (RequestBody) {Sensor[]} [sensors] an array containing the sensors of this senseBox. Only use if `model` is unspecified.
- * @apiParam (RequestBody) {String="hdc1080","bmp280","tsl45315","veml6070","sds 011"} [sensorTemplates] Specify which sensors should be included.
+ * @apiParam (RequestBody) {String="hdc1080","bmp280","tsl45315","veml6070","sds011"} [sensorTemplates] Specify which sensors should be included.
  * @apiParam (RequestBody) {Object} [mqtt] specify parameters of the MQTT integration for external measurement upload. Please see below for the accepted parameters
  * @apiParam (RequestBody) {Object} [ttn] specify parameters for the TTN integration for measurement from TheThingsNetwork.org upload. Please see below for the accepted parameters
  *
