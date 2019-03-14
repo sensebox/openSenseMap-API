2c852329dae307f811801135aaa0ae7a16d7cfbd
umut0 <umut0815@yahoo.com>
2019-03-14 15:18:00 +0100

    basic schemas for notification service

---
+++ packages/models/src/notification/notificationRule.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const { mongoose } = require('../db'),
+  { schema: sensorSchema, model: Sensor } = require('../sensor/sensor');
+
+
+//Sensor schema
+const notificationRuleSchema = new mongoose.Schema({
+  sensors: {
+    type: [sensorSchema],
+    required: [true, 'at least one sensor required.'],
+  },
+  activationThreshold: {
+    type: Number,
+    required: true
+  },
+  activatitonOperator: {
+    type: String,
+    enum: ['over', 'under', 'equal'],
+    required: true
+  },
+  activationTrigger: {
+    type: String,
+    enum: ['any', 'all'],
+    required: true
+  },
+  notificationChannel: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'NotificationChannel',
+    required: true
+  }],
+  active: {
+    type: Boolean,
+    required: true,
+    default: true
+  },
+  user: {
+    ref: 'User',
+    type: mongoose.Schema.Types.ObjectId
+  }
+});
+
+
+
+const notificationRuleModel = mongoose.model('NotificationRule', notificationRuleSchema);
+
+module.exports = {
+  schema: notificationRuleSchema,
+  model: notificationRuleModel
+};
