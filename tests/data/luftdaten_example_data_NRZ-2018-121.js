4450319cfb9137ba16e062bb7ee3ebe925805f73
ubergesundheit <ubergesundheit@googlemail.com>
2019-04-20 17:58:19 +0200

    add luftdaten test mock I found on my computer

---
+++ tests/data/luftdaten_example_data_NRZ-2018-121.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  software_version: 'NRZ-2018-121C',
+  sensordatavalues: [
+    {
+      value_type: 'SDS_P1',
+      value: '1.87'
+    },
+    {
+      value_type: 'SDS_P2',
+      value: '1.05'
+    },
+    {
+      value_type: 'BMP_pressure',
+      value: '100659.00'
+    },
+    {
+      value_type: 'BMP_temperature',
+      value: '26.80'
+    },
+    {
+      value_type: 'samples',
+      value: '924605'
+    },
+    {
+      value_type: 'min_micro',
+      value: '147'
+    },
+    {
+      value_type: 'max_micro',
+      value: '1849213'
+    },
+    {
+      value_type: 'signal',
+      value: '-52'
+    }
+  ]
+};
