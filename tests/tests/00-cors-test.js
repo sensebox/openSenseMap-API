303bd486d0357c4b2ecda08712133f07157e2f18
Matthias Pfeil <matthias.pfeil@wwu.de>
2022-03-07 17:32:41 +0100

    use chai-http

--- tests/tests/00-cors-test.js
+++ tests/tests/00-cors-test.js
@@ -2,35 +2,36 @@
 
 /* global describe it */
 
-const chakram = require('chakram'),
-  expect = chakram.expect;
+const chai = require('chai'),
+  chaiHttp = require('chai-http'),
+  expect = chai.expect;
+
+chai.use(chaiHttp);
 
 const BASE_URL = process.env.OSEM_TEST_BASE_URL;
 
 describe('openSenseMap API CORS test', function () {
   it('should always send out CORS headers', function () {
-    return chakram.get(BASE_URL)
-      .then(function (response) {
+    chai.request(BASE_URL)
+      .get('/')
+      .end(function (response) {
         expect(response).status(200);
         expect(response).to.have.header('access-control-allow-origin', '*');
         expect(response).to.have.header('access-control-allow-methods', 'GET, OPTIONS');
         expect(response).to.have.header('access-control-allow-headers', /authorization/i);
         expect(response).to.have.header('access-control-expose-headers', /content-disposition/i);
-
-        return chakram.wait();
       });
   });
 
   it('should send out CORS headers for preflight requests', function () {
-    return chakram.options(BASE_URL, { headers: { 'access-control-request-method': 'post' } })
-      .then(function (response) {
+    chai.request(BASE_URL, { headers: { 'access-control-request-method': 'post' } })
+      .options()
+      .end(function (response) {
         expect(response).status(204);
         expect(response).to.have.header('access-control-allow-origin', '*');
         expect(response).to.have.header('access-control-allow-methods', 'POST, OPTIONS');
         expect(response).to.have.header('access-control-allow-headers', /authorization/i);
         expect(response).to.have.header('access-control-expose-headers', /content-disposition/i);
-
-        return chakram.wait();
       });
   });
 });
