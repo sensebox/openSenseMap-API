12ec146d970099115d1e8be8cbbc7c43614d30a2
freds-dev <frebruc@gmail.com>
2023-07-06 22:10:47 +0800

    more eslint

--- packages/api/lib/helpers/notifications.js
+++ packages/api/lib/helpers/notifications.js
@@ -1,20 +1,22 @@
+'use strict';
+
 const { Novu } = require('@novu/node');
 const config = require('config');
 
-const options = { backendUrl: config.get('novu.backend_url') }
+const options = { backendUrl: config.get('novu.backend_url') };
 const api_key = config.get('novu.api_key');
 
 const novu = new Novu(api_key, options);
 
-const triggerNotification = async function triggerNotification(subscriberId, payload) {
-    novu.trigger('badge-notification-osem', {
-        to: {
-            subscriberId: subscriberId
-        },
-        payload: payload,
-    });
-}
+const triggerNotification = async function triggerNotification (subscriberId, payload) {
+  novu.trigger('badge-notification-osem', {
+    to: {
+      subscriberId: subscriberId
+    },
+    payload: payload,
+  });
+};
 
 module.exports = {
-    triggerNotification
-}
+  triggerNotification
+};
