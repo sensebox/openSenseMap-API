f6374da8714957135149909a978bf08d9962970b
Thiemann96 <erictg96@gmail.com>
2023-09-07 10:52:39 +0200

    adjusted getting tags function, renamed route

--- packages/api/lib/controllers/boxesController.js
+++ packages/api/lib/controllers/boxesController.js
@@ -489,18 +489,10 @@ const deleteBox = async function deleteBox (req, res, next) {
 
 const getAllTags = async function getAllTags (req, res, next) {
   try {
-    const output = [];
-    const boxes = await Box.find().exec();
-    boxes.map(box => {
-      const tags = box.grouptag;
-      tags.map(tag => {
-        const includes = output.includes(tag);
-        if (tag.length > 1 && !includes) {
-          output.push(tag);
-        }
-      });
-    });
-    res.send({ code: 'Ok', data: output });
+    const grouptags = await Box.find().distinct('grouptag')
+      .exec();
+
+    res.send({ code: 'Ok', data: grouptags });
   } catch (err) {
     handleError(err, next);
   }
