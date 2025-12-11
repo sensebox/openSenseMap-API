773dc52701b1e8501bdb7f77e7278fd17b46749f
lav-sen <lavinia.sen0@gmail.com>
2025-12-11 15:57:14 +0100

    add missing import

--- packages/models/src/box/sensorLayouts/index.js
+++ packages/models/src/box/sensorLayouts/index.js
@@ -19,7 +19,8 @@ const senseboxhome = require('./sensebox.home'),
   luftdaten_sps30_bme280 = require('./luftdaten/sps30.bme280'),
   luftdaten_sps30_sht3x = require('./luftdaten/sps30.shtx3'),
   hackair_home_v2 = require('./hackair/home.v2'),
-  addonFeinstaub = require('./addons/feinstaubAddon');
+  addonFeinstaub = require('./addons/feinstaubAddon'),
+  noiseMonitor = require('./noiseMonitor/noiseMonitor');
 
 /*
  * How to add a new senseBox model?
